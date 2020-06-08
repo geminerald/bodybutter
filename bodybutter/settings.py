70453141b73299ce5365c581eaef6127bf38fede
geminerald <zacdwarner@gmail.com>
2020-06-08 14:28:47 +0000

    corrected typo in settings py file

--- bodybutter/settings.py
+++ bodybutter/settings.py
@@ -125,4 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MESSAGE_STORAGE = 'django,contrib.messages.storage.session.SessionStorage'
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
